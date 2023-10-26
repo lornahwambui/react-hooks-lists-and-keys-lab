ea2af869e5074247f7e7544489ac91e1df1f0e3c
lornahWambuiGitau <lornah.gitau@student.moringaschool.com>
2023-10-26 09:03:52 +0300

    completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,9 +7,12 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((tech, index)=>
+        <span key={index}>{tech}</span>)}
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+
